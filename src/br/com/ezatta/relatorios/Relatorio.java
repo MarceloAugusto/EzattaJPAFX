cc12474cb9c0f704f89bf7ca3054b2cc656bfba0
marcelo <marceloaugusto16@gmail.com>
2015-07-27 17:53:38 -0300

    Relatorios -> Geral

---
+++ src/br/com/ezatta/relatorios/Relatorio.java
@@ -0,0 +1,124 @@
+package br.com.ezatta.relatorios;
+
+import br.com.ezatta.util.JPAUtil;
+import java.io.InputStream;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import javax.persistence.EntityManager;
+import javax.swing.JOptionPane;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import net.sf.jasperreports.view.JasperViewer;
+
+public class Relatorio {
+
+    public Relatorio() {
+    }
+
+    public JasperPrint gerar() throws SQLException {
+        JasperPrint rel = null;
+        try {
+            EntityManager em = new JPAUtil().getEntityManager();
+            //Connection con = em.unwrap(java.sql.Connection.class);
+            
+            java.sql.Connection con = em.unwrap(java.sql.Connection.class);
+            
+            HashMap map = new HashMap();
+            InputStream resource = getClass().getResourceAsStream("Geral.jasper");
+            rel = JasperFillManager.fillReport(resource, map, con);
+        } catch (JRException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return rel;
+    }
+
+    public JasperPrint RelatorioData(java.sql.Timestamp data1, java.sql.Timestamp data2) throws SQLException {
+        JasperPrint rel = null;
+        try {
+            EntityManager em = new JPAUtil().getEntityManager();
+            Connection con = em.unwrap(java.sql.Connection.class);
+            
+            HashMap map = new HashMap();
+
+            map.put("DataInicio", data1);
+            map.put("DataFinal", data2);
+            InputStream resource = getClass().getResourceAsStream("GeralDatas.jasper");
+            rel = JasperFillManager.fillReport(resource, map, con);
+
+            System.out.println(data1.getTime());
+        } catch (JRException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return rel;
+    }
+
+    public JasperPrint RelatorioDataLog(java.sql.Timestamp data1, java.sql.Timestamp data2) throws SQLException {
+        JasperPrint rel = null;
+        try {
+            EntityManager em = new JPAUtil().getEntityManager();
+            Connection con = em.unwrap(java.sql.Connection.class);
+            
+            HashMap map = new HashMap();
+
+            map.put("DataInicio", data1);
+            map.put("DataFinal", data2);
+            InputStream resource = getClass().getResourceAsStream("LogDatas.jasper");
+            rel = JasperFillManager.fillReport(resource, map, con);
+
+            System.out.println(data1.getTime());
+        } catch (JRException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return rel;
+    }
+
+    public JasperPrint RelatorioDataProduto(java.sql.Timestamp data1, java.sql.Timestamp data2, int idProduto) throws SQLException {
+        JasperPrint rel = null;
+        try {
+            
+            EntityManager em = new JPAUtil().getEntityManager();
+            Connection con = em.unwrap(java.sql.Connection.class);
+            
+            HashMap map = new HashMap();
+
+            map.put("DataInicio", data1);
+            map.put("DataFinal", data2);
+            map.put("Produto", idProduto);
+            InputStream resource = getClass().getResourceAsStream("GeralDatasProduto.jasper");
+            rel = JasperFillManager.fillReport(resource, map, con);
+
+        } catch (JRException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return rel;
+    }
+
+    public JasperPrint RelatorioDataBico(Timestamp datafim1, Timestamp datafim2, Integer id) {
+        JasperPrint rel = null;
+        try {
+            
+            EntityManager em = new JPAUtil().getEntityManager();
+            Connection con = em.unwrap(java.sql.Connection.class);
+            
+            HashMap map = new HashMap();
+
+            map.put("DataInicio", datafim1);
+            map.put("DataFinal", datafim2);
+            map.put("Produto", id);
+            InputStream resource = getClass().getResourceAsStream("/br/com/devmedia/Relatorio/GeralDatasBico.jasper");
+            rel = JasperFillManager.fillReport(resource, map, con);
+
+            //System.out.println(data1.getTime());
+        } catch (JRException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        return rel;
+    }
+
+}
---
+++ src/br/com/ezatta/teste/TesteRelatorios.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ezatta.teste;
+
+import br.com.ezatta.model.EzattaEmpresa;
+import br.com.ezatta.relatorios.Relatorio;
+import br.com.ezatta.util.JPAUtil;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+import org.hibernate.Session;
+
+//import org.hibernate.Session;
+//import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+//import org.hibernate.engine.spi.SessionFactoryImplementor;
+//import org.hibernate.connection.ConnectionProvider;
+//import org.hibernate.engine.SessionFactoryImplementor;
+/**
+ *
+ * @author marcelo
+ */
+public class TesteRelatorios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String path = "/home/marcelo/relatorios/";
+
+        JasperPrint relat;
+
+            System.out.println("path: " + path);
+
+            EntityManager em = new JPAUtil().getEntityManager();
+            em.getTransaction().begin();
+
+            Session hibernateSession = em.unwrap(Session.class);
+
+            hibernateSession.doWork(new org.hibernate.jdbc.Work() {
+
+                @Override
+                public void execute(Connection con) throws SQLException {
+                    try {
+                        HashMap map = new HashMap();
+                        JasperPrint rel = JasperFillManager.fillReport(path + "Geral.jasper", map, con);
+                        JasperViewer jrviewer = new JasperViewer(rel, false);
+                        jrviewer.show();
+                    } catch (JRException ex) {
+                        Logger.getLogger(TesteRelatorios.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                
+            });
+
+            TypedQuery<EzattaEmpresa> query = em.createNamedQuery("EzattaEmpresa.findAll", EzattaEmpresa.class);
+            List<EzattaEmpresa> ps = query.getResultList();
+            for (EzattaEmpresa p1 : ps) {
+                System.out.println("Nome: " + p1.getNome());
+            }
+
+       
+
+    }
+
+}
